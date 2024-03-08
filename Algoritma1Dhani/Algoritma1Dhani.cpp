c2ed23d5872e5b6c154ff3d5265d1db577604d87
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-03-08 11:04:10 +0700

    membuat BubbbleSortArray

--- Algoritma1Dhani/Algoritma1Dhani.cpp
+++ Algoritma1Dhani/Algoritma1Dhani.cpp
@@ -16,4 +16,31 @@ void input() {
 
 		}
 	}
+	cout << endl;
+	cout << "=====================" << endl;
+	cout << "Masukkan Elemen Array" << endl;
+	cout << "=====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> a[i];
+
+	}
+}
+
+void bubbleSortyArray() {
+	int pass = 1; 
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (a[j] > a[j + 1]) {
+				int temp;
+				temp = a[j];
+				a[j] = a[j + 1];
+
+			}
+		}
+		pass = pass + 1;
+
+	} while (pass <= n - 1);
+
 }
