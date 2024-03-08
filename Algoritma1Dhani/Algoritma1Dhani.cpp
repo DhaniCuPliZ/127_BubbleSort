08cb6b4891ed174a605fac8812158fa15b22675b
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-03-08 10:45:34 +0700

    Add project files.

---
+++ Algoritma1Dhani/Algoritma1Dhani.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+
+void input() {
+	while (true) {
+		cout << "masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			
+
+		}
+	}
+}
